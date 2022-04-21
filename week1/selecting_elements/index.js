c7c22aff67d9d753e5a983622a5d5b2ba587e83b
j-faherty <jorden.faherty@bryanuniversity.edu>
2022-04-20 20:41:28 -0500

    create selecting_elements folder with html and js files

---
+++ week1/selecting_elements/index.js
@@ -0,0 +1,9 @@
+var title = document.getElementById('Jorden');
+console.log(title);
+
+document.getElementById("H2-1").style.color = "yellow";
+document.getElementById("H2-1").style.fontStyle = "italic";
+document.querySelector("para").style.color = "blue";
+document.getElementById("H2-2").style.fontStyle = "italic";
+document.getElementById("H2-3").style.fontStyle = "italic";
+
