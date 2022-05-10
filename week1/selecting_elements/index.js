432637dd9bcea2da6c19b241925708adb056fab2
Jorden Faherty <99846255+j-faherty@users.noreply.github.com>
2022-05-10 11:21:24 -0500

    add content

--- week1/selecting_elements/index.js
+++ week1/selecting_elements/index.js
@@ -1,9 +1,3 @@
-var title = document.getElementById('Jorden');
-console.log(title);
-
-document.getElementById("H2-1").style.color = "yellow";
-document.getElementById("H2-1").style.fontStyle = "italic";
-document.querySelector("para").style.color = "blue";
-document.getElementById("H2-2").style.fontStyle = "italic";
-document.getElementById("H2-3").style.fontStyle = "italic";
-
+var introduction = document.getElementByTagName('h1')[0];
+h1.innerHTML = "Hello"
+console.log(introduction)
